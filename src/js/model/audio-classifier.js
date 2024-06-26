e3e549cf7f88f30b0df5b9f177626ee3e0497595
Luciano Ayres <lucianofarias@gmail.com>
2024-06-26 05:40:48 -0300

    refactor: refactor project structure and canvas window

--- src/js/model/audio-classifier.js
+++ src/js/model/audio-classifier.js
@@ -1,3 +1,7 @@
+const AUDIO_CLASSIFIER_MODEL_BASE_URL = "https://teachablemachine.withgoogle.com/models/8NWN8Sc2n/"
+const AUDIO_CLASSIFIER_MODEL_CHECKPOINT_URL = AUDIO_CLASSIFIER_MODEL_BASE_URL + "model.json" // model topology
+const AUDIO_CLASSIFIER_MODEL_METADATA_URL = AUDIO_CLASSIFIER_MODEL_BASE_URL + "metadata.json" // model metadata
+
 async function createModel() {
     const recognizer = speechCommands.create("BROWSER_FFT", undefined, AUDIO_CLASSIFIER_MODEL_CHECKPOINT_URL, AUDIO_CLASSIFIER_MODEL_METADATA_URL)
     await recognizer.ensureModelLoaded()
--- src/js/model/pose-classifier.js
+++ src/js/model/pose-classifier.js
@@ -1,3 +1,7 @@
+const POSE_CLASSIFIER_MODEL_BASE_URL = "https://teachablemachine.withgoogle.com/models/NX1Rf2z6f/"
+const POSE_CLASSIFIER_MODEL_CHECKPOINT_URL = POSE_CLASSIFIER_MODEL_BASE_URL + "model.json" // model topology
+const POSE_CLASSIFIER_MODEL_METADATA_URL = POSE_CLASSIFIER_MODEL_BASE_URL + "metadata.json" // model metadata
+
 let model, webcam, ctx, labelContainer, maxPredictions
 let currentState = "Idle"
 let intervalId
